7aeb997f7952e32fa9d944913be9f079b05acef2
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-02-07 13:10:27 +0300

    Update package version to 1.6.8: HitData rigidbody support removed

--- Runtime/Scripts/SCR_CoreUtility.cs
+++ Runtime/Scripts/SCR_CoreUtility.cs
@@ -550,7 +550,6 @@ namespace Core
         public readonly struct HitData
         {
             public readonly Collider Collider;
-            public readonly Rigidbody Rigidbody;
             public readonly Vector3 Point;
             public readonly Vector3 Normal;
             public readonly float Distance;
@@ -558,7 +557,6 @@ namespace Core
             public HitData(Collider collider, Vector3 point, Vector3 normal, float distance)
             {
                 Collider = collider;
-                Rigidbody = collider.attachedRigidbody;
                 Point = point;
                 Normal = normal;
                 Distance = distance;
