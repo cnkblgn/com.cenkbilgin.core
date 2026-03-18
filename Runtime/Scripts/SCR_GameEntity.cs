3081c356decfbb86c19e2e86c8a00ef3d2647a2b
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-03-18 13:42:07 +0300

    Update package version to 2.1.2: Core Update

--- Runtime/Scripts/SCR_GameEntity.cs
+++ Runtime/Scripts/SCR_GameEntity.cs
@@ -3,5 +3,19 @@ using UnityEngine;
 namespace Core
 {
     [DisallowMultipleComponent, SelectionBase]
-    public class GameEntity : MonoBehaviour { }
+    public class GameEntity : MonoBehaviour 
+    {
+        public Transform ThisTransform
+        {
+            get
+            {
+                if (thisTransform == null)
+                {
+                    thisTransform = transform;
+                }
+
+                return thisTransform;
+            }
+        } private Transform thisTransform = null;
+    }
 }
