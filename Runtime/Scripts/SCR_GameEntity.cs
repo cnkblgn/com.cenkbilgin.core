eedb031323925e8c35ac1a69f9193558097454e8
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-03-18 13:17:55 +0300

    Update package version to 2.1.1: Core Update

--- Runtime/Scripts/SCR_GameEntity.cs
+++ Runtime/Scripts/SCR_GameEntity.cs
@@ -1,55 +1,7 @@
-using System;
 using UnityEngine;
 
 namespace Core
 {
-    [SelectionBase]
-    [DisallowMultipleComponent]
-    [RequireComponent(typeof(Collider))]
-    public class GameEntity : MonoBehaviour
-    {
-        public event Action OnActivated = null;
-        public event Action OnDeactivated = null;
-
-        public Collider ThisCollider => thisCollider;
-        public bool IsPlayer => isPlayer;
-
-        [Header("_")]
-        [SerializeField] private bool isPlayer = false;
-
-        private Collider thisCollider = null;
-
-        protected virtual void Awake()
-        {
-            thisCollider = GetComponent<Collider>();
-
-            UpdateLayer();
-        }
-#if UNITY_EDITOR
-        private void Reset() => UpdateLayer();
-#endif
-        protected virtual void UpdateLayer() => gameObject.SetLayer(LayerMask.NameToLayer(isPlayer ? "Player" : "Entity"));
-
-        public void Override(bool isPlayer)
-        {
-            this.isPlayer = isPlayer;
-            UpdateLayer();
-        }
-        public void Activate()
-        {
-            OnActivated?.Invoke();
-
-            thisCollider.enabled = true;           
-
-            this.enabled = true;
-        }
-        public void Deactivate()
-        {
-            OnDeactivated?.Invoke();
-
-            thisCollider.enabled = false;
-
-            this.enabled = false;
-        }
-    }
+    [DisallowMultipleComponent, SelectionBase]
+    public class GameEntity : MonoBehaviour { }
 }
