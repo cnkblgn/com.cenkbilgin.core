18ac11662f09b2cd8c7995040845839890cc3ec4
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-07-31 12:11:51 +0300

    Update package version to 4.3.0: Core Update

--- Runtime/Animation/Scripts/SCR_PoseInstance.cs
+++ Runtime/Animation/Scripts/SCR_PoseInstance.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 namespace Core.Animation
 {
-    public class PoseInstance
+    public sealed class PoseInstance
     {
+        private Vector3 currentPose = Vector3.zero;
         private Vector3 currentVelocity = Vector3.zero;
-        public Vector3 Update(Vector3 from, PoseConfig target, float deltaTime)
-        {
-            if (target == null)
-            {
-                return Vector3.SmoothDamp(from, Vector3.zero, ref currentVelocity, 2.5f, float.PositiveInfinity, deltaTime);
-            }
 
-            return Vector3.SmoothDamp(from, target.Target, ref currentVelocity, target.Smoothness, float.PositiveInfinity, deltaTime);
+        public Vector3 Update(PoseConfig target, float deltaTime)
+        {
+            Vector3 targetValue = target != null ? target.Target : Vector3.zero;
+            float smoothness = target != null ? target.Smoothness : 2.5f;
+
+            currentPose = Vector3.SmoothDamp(currentPose, targetValue, ref currentVelocity, smoothness, 100f, deltaTime * 10f);
+
+            return currentPose;
+        }
+
+        public void Reset(Vector3 value = default)
+        {
+            currentPose = value;
+            currentVelocity = Vector3.zero;
         }
     }
 }
