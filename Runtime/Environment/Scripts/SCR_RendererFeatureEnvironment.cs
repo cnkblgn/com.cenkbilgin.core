bc817bcd1120c629596e396029c0a631583dfe04
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-08-11 23:55:14 +0300

    Update package version to 4.7.5: Core Update

--- Runtime/Environment/Scripts/SCR_RendererFeatureEnvironment.cs
+++ Runtime/Environment/Scripts/SCR_RendererFeatureEnvironment.cs
@@ -1,17 +1,60 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 namespace Core.Environment
 {
     internal sealed class RendererFeatureEnvironment : ScriptableRendererFeature
     {
-        [Info("Use environment system to update global constants!\nRequires depth texture and injection point 'AfterOpaques'\nAlso this feature disables unity fog!")]
-        [SerializeField, Required] private Material fogMaterial;
-        [SerializeField, Required] private Material skyMaterial;
+        private const string FOG_SHADER_PATH = "Hidden/FX_FullscreenFog";
+        private const string SKY_SHADER_PATH = "Hidden/FX_UnlitSky";
 
+        [Info("Use environment system to update global constants!\nRequires depth texture and injection point 'AfterOpaques'\nAlso this feature disables unity fog!")]
+        [SerializeField, ReadOnly] private bool hasInitialized;
+
+        private Material fogMaterial;
+        private Material skyMaterial;
         private RendererPassEnvironment pass;
 
-        public override void Create() => pass = new();
+        public override void Create()
+        {
+            Shader fogShader = Shader.Find(FOG_SHADER_PATH);
+            Shader skyShader = Shader.Find(SKY_SHADER_PATH);
+
+            if (fogShader != null)
+            {
+                fogMaterial = CoreUtils.CreateEngineMaterial(fogShader);
+                hasInitialized = true;
+            }
+            else
+            {
+                Debug.LogError($"Fog shader [{FOG_SHADER_PATH}] not found?");
+                hasInitialized = false;
+                return;
+            }
+
+            if (skyShader != null)
+            {
+                skyMaterial = CoreUtils.CreateEngineMaterial(skyShader);
+                hasInitialized = true;
+            }
+            else
+            {
+                Debug.LogError($"Sky shader [{SKY_SHADER_PATH}] not found?");
+                hasInitialized = false;
+                return;
+            }
+
+            pass = new();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            CoreUtils.Destroy(fogMaterial);
+            CoreUtils.Destroy(skyMaterial);
+
+            fogMaterial = null;
+            skyMaterial = null;
+        }
 
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
