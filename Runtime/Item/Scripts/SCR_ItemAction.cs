f0b67a10059ecc5b6d50b8d27f4246cbbcd8288f
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-09-04 21:34:29 +0300

    Update package version to 5.9.3: Core Update

--- Runtime/Item/Scripts/SCR_ItemAction.cs
+++ Runtime/Item/Scripts/SCR_ItemAction.cs
@@ -6,7 +6,6 @@ namespace Core.Item
     public abstract class ItemAction
     {
         public abstract string GetName();
-        public abstract bool CanApply(in ItemActionContext context);
         public abstract ItemActionResult Apply(in ItemActionContext context);
     }
 }
