f11b9a3182830d9fdad4e92707a84902dc059971
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-09-09 01:03:48 +0300

    Update package version to 6.4.1: Core Update

--- Runtime/UI/Scripts/SCR_UIContextMenuController.cs
+++ Runtime/UI/Scripts/SCR_UIContextMenuController.cs
@@ -18,7 +18,7 @@ namespace Core.UI
         [SerializeField, Required] private UIContextItemView viewTemplate;
 
         private Canvas thisCanvas;
-        private UIContextMenuHandle thisHandle;
+        private UIContextHandle thisHandle;
         private readonly List<UIContextItemView> thisItems = new();
 
         private void Awake()
@@ -47,7 +47,7 @@ namespace Core.UI
 
         public void OnSelected() => Hide();
 
-        public UIContextMenuHandle Show(in UIContextMenuContext ctx)
+        public UIContextHandle Show(in UIContextMenuContext ctx)
         {
             if (thisHandle != default)
             {
@@ -62,7 +62,7 @@ namespace Core.UI
 
             return thisHandle = new(Guid.NewGuid());
         }
-        public void Hide(UIContextMenuHandle handle)
+        public void Hide(UIContextHandle handle)
         {
             if (thisHandle != handle)
             {
