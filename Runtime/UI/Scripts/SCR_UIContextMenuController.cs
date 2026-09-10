9626ed21c63bc2545317e9f4704cfe555bf0e43b
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-09-10 19:36:35 +0300

    Update package version to 6.5.0: Core Update

--- Runtime/UI/Scripts/SCR_UIContextMenuController.cs
+++ Runtime/UI/Scripts/SCR_UIContextMenuController.cs
@@ -59,7 +59,7 @@ namespace Core.UI
             thisCanvas.Show();
             ManagerUI.Instance.ShowCursor();
 
-            root.localPosition = ctx.Position + (Vector2.right * root.rect.width);
+            root.position = ctx.Position + (Vector2.right * root.rect.width);
 
             Populate(in ctx);
 
