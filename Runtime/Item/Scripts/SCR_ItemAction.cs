1948dfd55349a0dbb28cabe18594a51877076dc4
cnkblgn <91436084+cnkblgn@users.noreply.github.com>
2026-09-11 17:34:07 +0300

    Update package version to 6.6.1: Core Update

--- Runtime/Item/Scripts/SCR_ItemAction.cs
+++ Runtime/Item/Scripts/SCR_ItemAction.cs
@@ -6,7 +6,7 @@ namespace Core.Item
     public abstract class ItemAction
     {
         public abstract string GetName();
-        public abstract bool IsValid();
+        public abstract bool IsValid(in ItemActionContext context);
         public abstract ItemActionResult Apply(in ItemActionContext context);
     }
 }
